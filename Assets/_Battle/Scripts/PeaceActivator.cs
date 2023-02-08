caacf80dff31089daa47b62516f09dd6502a5c53
Павел Тесленко <84148561+1wz@users.noreply.github.com>
2023-02-08 21:39:26 +0300

    Initial commit

---
+++ Assets/_Battle/Scripts/PeaceActivator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BattleScripts
+{
+    class PeaceActivator: IEnemy
+    {
+        Button _passPeacefullyButton;
+        const int maxPeaceRate = 2;
+        public PeaceActivator(Button passPeacefullyButton)
+        {
+            _passPeacefullyButton = passPeacefullyButton;
+        }
+
+        public void Update(PlayerData playerData)
+        {
+            switch (playerData.DataType)
+            {
+                case DataType.CrimeRate:
+                    ChangeCrimeRate(playerData.Value);
+                    break;
+
+            }
+
+        }
+
+        private void ChangeCrimeRate(int crimeRateValue)
+        {
+            if (crimeRateValue > maxPeaceRate) _passPeacefullyButton.gameObject.SetActive(false);
+            else _passPeacefullyButton.gameObject.SetActive(true);
+        }
+    }
+    
+}
